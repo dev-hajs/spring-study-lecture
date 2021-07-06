c39998579455d94630ece87243ff053c47394346
하지수 <dev.hajs@gmail.com>
2021-07-06 20:42:49 +0900

    옵션 처리 -. 스프링 빈으로 관리가 안되는 객체를 주입하고자 하면 오류가 발생한다 -. 이 때 오류없이 코드가 동작하도록 하는 3가지 방법에 대해서 학습하자 -. 첫 번째는 autowired 의 required 속성값을 false 로 주는 것 -. 두 번째는 nullable 어노테이션 사용 -. 마지막 세 번째는 optional class 사용

--- src/main/java/hello/core/AutoAppConfig.java
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -1,5 +1,8 @@
 package hello.core;
 
+import hello.core.member.MemberRepository;
+import hello.core.member.MemoryMemberRepository;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
@@ -9,4 +12,11 @@ import org.springframework.context.annotation.FilterType;
         excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
 )
 public class AutoAppConfig {
+/*
+* 중복으로 등록된 빈 충돌해결 코드는 테스트코드 실패로 주석처리
+    @Bean(name = "memoryMemberRepository")
+    MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }
+*/
 }
